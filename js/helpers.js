b391558035bef1d19c9a875e8c7f8766c2924262
seidenberger <158757732+seidenberger@users.noreply.github.com>
2024-11-20 11:27:44 +0100

    helpers.js JSDoc

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,12 @@
+let orientationMediaQuery = window.matchMedia("(orientation: portrait)");
+
+/**
+ * Checks the screen orientation and adjusts the visibility of game elements accordingly.
+ *
+ * This function uses `window.matchMedia` to determine if the screen is in portrait mode.
+ * Based on the orientation, it updates the visibility of elements like the canvas container,
+ * the orientation message, the game name, and game control instructions.
+ */
 function checkOrientationWithMediaQuery() {
   let isPortrait = window.matchMedia("(orientation: portrait)").matches;
   let canvasContainer = document.getElementById("containerCanvas");
@@ -25,8 +34,6 @@ function checkOrientationWithMediaQuery() {
   }
 }
 
-let orientationMediaQuery = window.matchMedia("(orientation: portrait)");
-
 orientationMediaQuery.addEventListener(
   "change",
   checkOrientationWithMediaQuery
@@ -34,16 +41,35 @@ orientationMediaQuery.addEventListener(
 
 window.addEventListener("load", checkOrientationWithMediaQuery);
 
+/**
+ * Displays the Impressum (legal notice) and data privacy information.
+ *
+ * This function calls `showImpressumDataPrivacy()` to display the Impressum and data
+ * privacy details, and then calls `backShow()` to display the back button or hide certain UI elements.
+ */
 function impressum() {
   showImpressumDataPrivacy();
   backShow();
 }
 
+/**
+ * Displays the data privacy information.
+ *
+ * This function calls `showImpressumDataPrivacy()` to display the data privacy details,
+ * and then calls `backShow()` to display the back button or hide certain UI elements for navigation.
+ */
 function dataPrivacy() {
   showImpressumDataPrivacy();
   backShow();
 }
 
+/**
+ * Displays the Impressum (legal notice) and data privacy information by hiding game elements.
+ *
+ * This function hides the game-related elements such as the start display, the canvas container,
+ * and the overlay by adding `hidden` and `hidden_imp` classes. These actions ensure the game
+ * elements are not visible while the Impressum and data privacy details are shown.
+ */
 function showImpressumDataPrivacy() {
   let startDisplay = document.getElementById("startDisplay");
   let containerCanvas = document.getElementById("containerCanvas");
